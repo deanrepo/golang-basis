43fbd35f1f6f5c2d56ef3699bf2f5f829f3748fc
Jerry Zhang <greenminghui@gmail.com>
2019-03-02 13:50:27 +0800

    time practise

--- time/time.go
+++ time/time.go
@@ -6,14 +6,14 @@ import (
 	"time"
 )
 
-var week time.Duration
+// var week time.Duration
 
 func main() {
-	t := time.Now()
+	// t := time.Now()
 	// fmt.Println(t)
 	// fmt.Printf("%02d.%02d.%4d\n", t.Day(), t.Month(), t.Year())
 
-	t = time.Now().UTC()
+	// t = time.Now().UTC()
 	// fmt.Println(t)
 	// fmt.Println(time.Now())
 
@@ -23,9 +23,21 @@ func main() {
 	// fmt.Println(weekFromNow)
 
 	// formatting times:
-	fmt.Println(t.Format(time.RFC822))
-	fmt.Println(t.Format(time.ANSIC))
-	fmt.Println(t.Format("02 Jan 2006 15:04"))
-	s := t.Format("20060102")
-	fmt.Println(t, "=>", s)
+	// fmt.Println(t.Format(time.RFC822))
+	// fmt.Println(t.Format(time.ANSIC))
+	// fmt.Println(t.Format("02 Jan 2006 15:04"))
+	// s := t.Format("20060102")
+	// fmt.Println(t, "=>", s)
+
+	timeTest1()
+}
+
+func timeTest1() {
+	t := time.Now().Local()
+	td := time.Date(2009, time.February, 23, 0, 0, 0, 0, t.Location())
+	td1 := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+
+	fmt.Println(t)
+	fmt.Println(td)
+	fmt.Println(td1)
 }
